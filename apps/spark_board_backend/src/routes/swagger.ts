4e958889796bac8aa7ffe7f7ea5e5967319a07ef
Albin <56157868+albinpk@users.noreply.github.com>
2024-11-18 23:29:37 +0530

    chore: add authorization option to swagger ui

--- apps/spark_board_backend/src/routes/swagger.ts
+++ apps/spark_board_backend/src/routes/swagger.ts
@@ -8,12 +8,20 @@ const option: swaggerJsdoc.Options = {
       title: "API for SparkBoard",
       version: "1.0.0",
     },
-    // tags: [
-    //   {
-    //     name: "Auth",
-    //     description: "Authentication related endpoints",
-    //   },
-    // ],
+    // tags: [{ name: "auth" }, { name: "user" }],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          in: "header",
+          name: "Authorization",
+          description: "Bearer token to access these api endpoints",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
     servers: [
       {
         url: "http://localhost:3000/api",
