cd6f5865c9a1e7fc8177cebceca7260e0808b895
Albin <56157868+albinpk@users.noreply.github.com>
2024-11-26 20:51:07 +0530

    fix: enable cors

--- apps/spark_board_backend/src/server.ts
+++ apps/spark_board_backend/src/server.ts
@@ -8,10 +8,12 @@ import { errorHandler } from "./middlewares/errorHandler";
 import { notFoundHandler } from "./middlewares/notFound";
 import { apiRoutes } from "./routes/api/apiRoutes";
 import { swaggerRoutes } from "./swagger/swagger";
+import cors from "cors";
 
 const port = process.env.PORT;
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api-docs", ...swaggerRoutes);
