14e8026d7f9ffdcd7b9eab370e6075a6cf5f558b
Albin <56157868+albinpk@users.noreply.github.com>
2024-11-30 17:26:01 +0530

    feat: collapsible sidebar

--- apps/spark_board_frontend/lib/modules/dashboard/project_shell.dart
+++ apps/spark_board_frontend/lib/modules/dashboard/project_shell.dart
@@ -1,8 +1,6 @@
-import 'package:go_router/go_router.dart';
-
 import '../../utils/common.dart';
-import '../../widgets/nested_navigation_rail.dart';
 import '../project_bar/project_bar_view.dart';
+import '../sidebar/sidebar_view.dart';
 
 /// Project shell widget for go router.
 class ProjectShell extends StatelessWidget {
@@ -20,105 +18,20 @@ class ProjectShell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final url = GoRouter.of(context).state!.uri.toString();
-
-    bool isStartsWith(String path) =>
-        url.startsWith('/projects/$projectId/$path');
-
     return Material(
-      child: Column(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ProjectBarView(projectId: projectId),
+          SidebarView(projectId: projectId),
           Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Column(
               children: [
-                SizedBox(
-                  width: 200,
-                  child: SingleChildScrollView(
-                    padding: const EdgeInsets.all(Margin.medium),
-                    child: Column(
-                      children: [
-                        _buildTile(
-                          context: context,
-                          iconData: Icons.checklist,
-                          selected: isStartsWith('tasks'),
-                          label: 'Tasks',
-                          onTap: () {
-                            TasksRoute(projectId: projectId).go(context);
-                          },
-                        ),
-                        _buildTile(
-                          context: context,
-                          selected: isStartsWith('user'),
-                          iconData: Icons.people_alt_outlined,
-                          label: 'Users',
-                          onTap: () {
-                            UsersRoute(projectId: projectId).go(context);
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                if (false)
-                  NestedNavigationRail(
-                    items: [
-                      NavItem(
-                        icon: const Icon(Icons.checklist),
-                        isSelected: isStartsWith('tasks'),
-                        label: 'Tasks',
-                        onTap: () {
-                          TasksRoute(projectId: projectId).go(context);
-                        },
-                      ),
-                      NavItem(
-                        icon: const Icon(Icons.person),
-                        isSelected: isStartsWith('users'),
-                        label: 'Users',
-                        onTap: () {
-                          UsersRoute(projectId: projectId).go(context);
-                        },
-                      ),
-                    ],
-                  ),
+                ProjectBarView(projectId: projectId),
                 Expanded(child: child),
               ],
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildTile({
-    required BuildContext context,
-    required String label,
-    required IconData iconData,
-    required VoidCallback onTap,
-    required bool selected,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: Margin.xSmall),
-      child: ListTile(
-        dense: true,
-        horizontalTitleGap: 6,
-        leading: Icon(iconData, size: 18),
-        selected: selected,
-        selectedTileColor: context.cs.surfaceContainer,
-        selectedColor: context.cs.onSurface,
-        minVerticalPadding: 0,
-        shape: RoundedRectangleBorder(
-          side: BorderSide(
-            color: selected
-                ? context.cs.surfaceContainerHighest
-                : Colors.transparent,
-          ),
-          borderRadius: BorderRadius.circular(Margin.medium),
-        ),
-        onTap: onTap,
-        visualDensity: const VisualDensity(vertical: -4),
-        title: Text(label),
       ),
     );
   }
