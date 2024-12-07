07e6713747fc5a0ea7a05378d5590e2f7ab74c0d
Albin PK <56157868+albinpk@users.noreply.github.com>
2024-12-07 20:58:26 +0530

    feat: CRUD api for `staff` (#21)

--- apps/spark_board_backend/src/routes/api/v1/v1Routes.ts
+++ apps/spark_board_backend/src/routes/api/v1/v1Routes.ts
@@ -4,6 +4,7 @@ import { good } from "../../../utils/response";
 import { tryCatch } from "../../../utils/tryCatch";
 import { authRoute } from "./auth/authRoutes";
 import { projectsRoute } from "./projects/projectsRoute";
+import { staffRoute } from "./staff/staffRoute";
 import { usersRoute } from "./users/usersRoute";
 
 /**
@@ -25,5 +26,8 @@ v1.use("/users", authGuard, usersRoute);
 
 // /api/v1/projects
 v1.use("/projects", authGuard, projectsRoute);
+
+// /api/v1/staff
+v1.use("/staff", authGuard, staffRoute);
 
 export { v1 as v1Route };
