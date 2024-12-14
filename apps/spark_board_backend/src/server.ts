8510a99eddae8a1a8c7d1cb1235249f75de73560
Albin <56157868+albinpk@users.noreply.github.com>
2024-12-14 21:17:28 +0530

    build: set static files path

--- apps/spark_board_backend/src/server.ts
+++ apps/spark_board_backend/src/server.ts
@@ -2,19 +2,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
 import { logHandler } from "./middlewares/apiLogger";
 import { errorHandler } from "./middlewares/errorHandler";
 import { notFoundHandler } from "./middlewares/notFound";
 import { apiRoutes } from "./routes/api/apiRoutes";
 import { swaggerRoutes } from "./swagger/swagger";
-import cors from "cors";
 
 const port = process.env.PORT;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 app.use("/api-docs", ...swaggerRoutes);
 
