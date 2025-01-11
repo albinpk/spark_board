94ca4b2b9faa0a8060fddace4eec6db363dc3d67
Albin <56157868+albinpk@users.noreply.github.com>
2025-01-11 13:00:00 +0530

    chore: update sed command for cross-platform compatibility in preVersion.sh

--- scripts/preVersion.sh
+++ scripts/preVersion.sh
@@ -12,7 +12,17 @@ packageJson="apps/spark_board_backend/package.json"
 packageLock="apps/spark_board_backend/package-lock.json"
 
 # Updating the version in package.json
-sed -i "/\"version\":/c\  \"version\": \"$newVersion\"," $packageJson
+
+# old (works on linux)
+# sed -i "/\"version\":/c\  \"version\": \"$newVersion\"," $packageJson
+
+# Cross-platform sed command
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e "/\"version\":/s/.*/  \"version\": \"$newVersion\",/" "$packageJson"
+else
+  sed -i -e "/\"version\":/s/.*/  \"version\": \"$newVersion\",/" "$packageJson"
+fi
+
 # npm install for updating package-lock.json
 npm --prefix "./apps/spark_board_backend" install
 
