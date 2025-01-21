e5f3e9ba68bc17afddbdfec08442052a47a3e9cb
Albin PK <56157868+albinpk@users.noreply.github.com>
2025-01-21 23:17:53 +0530

    feat: create new staff (#40)

--- apps/spark_board_frontend/lib/modules/staff/staff_state.dart
+++ apps/spark_board_frontend/lib/modules/staff/staff_state.dart
@@ -1,37 +1,20 @@
 import 'dart:async';
 
+import '../../providers/staff_list_provider.dart';
 import '../../services/api/models/staffs_response.dart';
 import '../../utils/common.dart';
 import 'staff_view.dart';
+import 'widgets/create_staff_dialog.dart';
 
 class StaffState extends CoraConsumerState<StaffView> with ObsStateMixin {
-  @override
-  void initState() {
-    super.initState();
-    _getStaffs();
-  }
-
-  late final staffs = obs<List<StaffResponse>>([]);
-
-  Future<void> _getStaffs() async {
-    final (err, data) = await ref.api.staffs(cancelToken: cancelToken()).go();
-    if (err != null) return AppSnackbar.error(err);
-
-    staffs.value = data!.data;
-  }
-
   Future<void> updateName(StaffResponse staff, String name) async {
+    if (staff.name == name) return;
     final (err, data) = await ref.api.updateStaff(
       staffId: staff.staffId,
-      body: {
-        'name': name.trim(),
-      },
+      body: {'name': name},
     ).go();
-
     if (err != null) return AppSnackbar.error(err);
-
-    staffs[staffs.indexOf(staff)] = data!.data;
-    staffs.refresh();
+    ref.invalidate(staffListProvider);
   }
 
   Future<void> deleteStaff(StaffResponse staff) async {
@@ -40,11 +23,18 @@ class StaffState extends CoraConsumerState<StaffView> with ObsStateMixin {
           staffId: staff.staffId,
         )
         .go();
-
     if (err != null) return AppSnackbar.error(err);
+    ref.invalidate(staffListProvider);
+  }
 
-    staffs
-      ..remove(staff)
-      ..refresh();
+  Future<void> createStaff(CreateStaffDialogResult result) async {
+    final (err, data) = await ref.api.createStaff(
+      body: {
+        'name': result.name,
+        'email': result.email,
+      },
+    ).go();
+    if (err != null) return AppSnackbar.error(err);
+    ref.invalidate(staffListProvider);
   }
 }
