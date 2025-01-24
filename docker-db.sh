c24cfcfef3799fc9a7c5f24ff57b53c635819052
Albin <56157868+albinpk@users.noreply.github.com>
2025-01-24 21:19:51 +0530

    chore: persist PostgreSQL data using a Docker volume

--- docker-db.sh
+++ docker-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run postgres container
-docker run -p 5432:5432 --name pg -e POSTGRES_PASSWORD=1234 -d postgres
+docker run -p 5432:5432 --name pg -e POSTGRES_PASSWORD=1234 -v spark_board_pg_data:/var/lib/postgresql/data -d postgres
 
 sleep 1
 
