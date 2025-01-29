4fc1564edfc5e5eb2cf2a8a2eb61e7dc484f066d
Albin <56157868+albinpk@users.noreply.github.com>
2025-01-29 23:05:10 +0530

    feat: implement shimmer loading effect for projects view

--- apps/spark_board_frontend/lib/widgets/shimmer.dart
+++ apps/spark_board_frontend/lib/widgets/shimmer.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 import '../utils/common.dart';
 
 class Shimmer<T> extends StatelessWidget {
@@ -5,20 +7,39 @@ class Shimmer<T> extends StatelessWidget {
     required this.value,
     required this.mock,
     required this.builder,
+    this.ignoreContainers,
     super.key,
   });
 
   final AsyncValue<T> value;
   final T mock;
   final Widget Function(T data) builder;
+  final bool? ignoreContainers;
 
   @override
   Widget build(BuildContext context) {
+    final value = this.value.unwrapPrevious();
     final data = value.valueOrNull ?? mock;
-    // TODO(albin): error
-    if (value.hasError) return const Text('error');
+
+    if (value.hasError) {
+      return Center(
+        child: kDebugMode
+            ? Text(
+                value.error.toString(),
+                style: context.bodyMedium.copyWith(
+                  color: context.cs.error,
+                ),
+              )
+            : Text(
+                'Something went wrong',
+                style: context.bodyMedium.fade(),
+              ),
+      );
+    }
+
     return Skeletonizer(
       enabled: value.isLoading,
+      ignoreContainers: ignoreContainers,
       child: builder(data),
     );
   }
